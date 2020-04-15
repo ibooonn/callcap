30ceed2454457dddb9924616785c58b4c354f48a
ibooonn <aenirestu@gmail.com>
2020-04-15 23:32:46 +0700

    fix calc logic

--- calc.js
+++ calc.js
@@ -1,5 +1,3 @@
-
-
 const add = (a, b) => {
   if (isNaN(a) || isNaN(b)) return false;
   if (Number.isInteger(a) !== true || Number.isInteger(b) !== true) {
@@ -9,10 +7,8 @@ const add = (a, b) => {
 };
 const print = (text) => {
   console.log(text);
-
 }
 
-const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
